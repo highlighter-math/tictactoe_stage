24c142effd1d284a806f20121099985eb702243d
highlighter-math <yumimon93@gmail.com>
2025-07-06 17:22:36 +0900

    commit

--- src/app/problem1/page.tsx
+++ src/app/problem1/page.tsx
@@ -4,12 +4,15 @@ import Grid from "@/components/Grid";
 import { Fab } from '@mui/material';
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 
+const prod = process.env.NODE_ENV === "production";
+const basePath = prod ? "/tictactoe_stage" : "";
+
 export default function HomePage() {
   return (
     <main style={{ padding: "20px" }}>
       <Grid />
       <Fab
-      href="/"
+      href={basePath+"/"}
       color="primary"
       aria-label="add"
       sx={{
