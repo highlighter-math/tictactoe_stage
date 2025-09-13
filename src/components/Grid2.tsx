007c3bea4719bdb2b964e74567773ac296c6385b
28q <79777472+28q@users.noreply.github.com>
2025-09-14 04:04:26 +0900

    Update Grid2.tsx

--- src/components/Grid2.tsx
+++ src/components/Grid2.tsx
@@ -2,15 +2,15 @@ import GridCell from "./GridCell";
 import "./grid.css";
 
 const cells = [
-  { answer: "アペリー定数", image: "アペリー定数.png" },
-  { answer: "アボガドロ定数", image: "アボガドロ定数.png" },
-  { answer: "オイラー定数", image: "オイラー定数.png" },
-  { answer: "ファラデー定数", image: "ファラデー定数.png" },
-  { answer: "プランク定数", image: "プランク定数.png" },
-  { answer: "円周率", image: "円周率.png" },
-  { answer: "光速", image: "光速.png" },
-  { answer: "標準気圧", image: "標準気圧.png" },
-  { answer: "万有引力定数", image: "万有引力定数.png" }
+  { answer: "ウクライナ", image: "ウクライナ.png" },
+  { answer: "インド", image: "インド.png" },
+  { answer: "カナダ", image: "カナダ.png" },
+  { answer: "タイ", image: "タイ.png" },
+  { answer: "エルサルバドル", image: "エルサルバドル.png" },
+  { answer: "ベルギー", image: "ベルギー.png" },
+  { answer: "コートジボワール", image: "コートジボワール.png" },
+  { answer: "カタール", image: "カタール.png" },
+  { answer: "ロシア", image: "ロシア.png" }
 ];
 
 const Grid2 = () => {
