a4b36eb862fb7a588a507f108fcfc7a9f542beed
28q <79777472+28q@users.noreply.github.com>
2025-10-30 12:30:52 +0900

    Update Grid2.tsx

--- src/components/Grid2.tsx
+++ src/components/Grid2.tsx
@@ -2,15 +2,15 @@ import GridCell from "./GridCell";
 import "./grid.css";
 
 const cells = [
-  { answer: "ツバル", image: "ツバル.png" },
-  { answer: "ボリビア", image: "ボリビア.png" },
-  { answer: "日本", image: "日本.png" },
-  { answer: "デンマーク", image: "デンマーク.png" },
-  { answer: "スウェーデン", image: "スウェーデン.png" },
-  { answer: "インド", image: "インド.png" },
-  { answer: "モナコ", image: "モナコ.png" },
+  { answer: "パリ", image: "パリ.png" },
+  { answer: "モスクワ", image: "モスクワ.png" },
+  { answer: "ベルリン", image: "ベルリン.png" },
+  { answer: "ローマ", image: "ローマ.png" },
+  { answer: "アテネ", image: "アテネ.png" },
+  { answer: "ペキン", image: "ペキン.png" },
+  { answer: "ウランバートル", image: "ウランバートル.png" },
   { answer: "トウキョウ", image: "トウキョウ.png" },
-  { answer: "バチカン市国", image: "バチカン市国.png" }
+  { answer: "ロンドン", image: "ロンドン.png" }
 ];
 
 const Grid2 = () => {
